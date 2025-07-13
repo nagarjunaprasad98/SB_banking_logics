40f1ae6817c76a1f3863803525c1385be1b7f758
nagarjunaprasad98 <nagarjunaprasad99@gmail.com>
2025-07-13 11:52:07 +0530

    addcomment

--- src/main/java/com/relationalMapper/RelationalMapper.java
+++ src/main/java/com/relationalMapper/RelationalMapper.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//Hai nag
 public class RelationalMapper {
 
 	public static void main(String[] args) {
